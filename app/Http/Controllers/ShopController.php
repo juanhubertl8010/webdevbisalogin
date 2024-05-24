6048587e72c74e659dfef4eb8839afd5806bc42b
juanhubertl8010 <127651844+juanhubertl8010@users.noreply.github.com>
2024-05-24 11:37:48 +0700

    shop udah jadi sementara

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Catalog;
 class ShopController extends Controller
 {
     public function show()
     {
-        return view('shop');
+        $catalogItems = Catalog::simplepaginate(9);
+
+        // Pass the catalog items to the view
+        return view('shop', ['catalogItems' => $catalogItems]);
     }
 }
